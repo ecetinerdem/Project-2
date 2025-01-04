6f962699ee0dfc9de3b94b35d7f09dad37676d97
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-04 21:09:25 +0400

    Update CustomerController.java

--- src/main/java/com/workintech/s18d4/controller/CustomerController.java
+++ src/main/java/com/workintech/s18d4/controller/CustomerController.java
@@ -1,11 +1,13 @@
 package com.workintech.s18d4.controller;
 
 import com.workintech.s18d4.dto.CustomerResponse;
+import com.workintech.s18d4.entity.Customer;
 import com.workintech.s18d4.service.CustomerService;
+import com.workintech.s18d4.util.Converter;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/customer")
@@ -14,7 +16,22 @@ public class CustomerController {
     private final CustomerService customerService;
 
     @GetMapping
-    public CustomerResponse findAll() {
+    public List<CustomerResponse> findAll() {
         return Converter.convertCustomerResponse(customerService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public CustomerResponse findById(@PathVariable("id") Long id) {
+        return Converter.convertCustomerResponse(customerService.findById(id));
+    }
+
+    @PostMapping
+    public CustomerResponse save(@RequestBody Customer customer) {
+        return Converter.convertCustomerResponse(customerService.save(customer));
+    }
+
+    @DeleteMapping("/{id}")
+    public CustomerResponse delete(@PathVariable("id") Long id) {
+        return Converter.convertCustomerResponse(customerService.delete(id));
     }
 }
