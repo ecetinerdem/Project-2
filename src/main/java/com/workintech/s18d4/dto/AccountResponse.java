7d9ccfcc8d9e08c9c37bd44534316d5eb3db9890
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-04 17:45:37 +0400

    add adress dto

--- src/main/java/com/workintech/s18d4/dto/AccountResponse.java
+++ src/main/java/com/workintech/s18d4/dto/AccountResponse.java
@@ -1,4 +1,4 @@
 package com.workintech.s18d4.dto;
 
-public record AccountResponse(Long id, String accountName, Double moneyAmount, CustomerResponse customerResponse) {
+public record AccountResponse(Long id, String accountName, Double moneyAmount, CustomerResponse customerResponse, AddressResponse addressResponse) {
 }
---
+++ src/main/java/com/workintech/s18d4/dto/AddressResponse.java
@@ -0,0 +1,4 @@
+package com.workintech.s18d4.dto;
+
+public record AddressResponse(Long id, String city, String country) {
+}
